f1c1a07e991cde36edc43be92d4891ed1cfbc580
nguyenductho <nguyenductho89@ominext.com>
2021-06-10 14:54:19 +0700

    first layout

--- Test/CollectionViewCell2.swift
+++ Test/CollectionViewCell2.swift
@@ -15,35 +15,12 @@ class CollectionViewCell2: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.gray.cgColor
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-
-    }
-
-    func setupStackView(_ datas: [String], _ index: Int, _ widthsNew: [CGFloat]) {
-        self.stackView.arrangedSubviews.forEach({$0.removeFromSuperview()})
-        datas.enumerated().forEach { index , item in
-            let customView = UINib(nibName: "ViewCollection", bundle: nil)
-                .instantiate(withOwner: nil, options: nil)
-                .first as? ViewCollection
-            guard let view = customView else {return}
-            view.fillData(text: item)
-//            if index == 0 {
-//                view.csWidthView.isActive = false
-//                view.closure = {[weak self] width in
-//                    self?.closure?(width)
-//                }
-//            } else {
-//                view.csWidthView.isActive = true
-//                if !widthsNew.isEmpty {
-//                    view.csWidthView.constant = widthsNew[index]
-//                }
-//            }
-            stackView.addArrangedSubview(view)
-        }
 
     }
 }
--- Test/TableViewCell.swift
+++ Test/TableViewCell.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var csWidthTableView: NSLayoutConstraint!
-
     @IBOutlet weak var collectionView: UICollectionView!
 
     let drugNames = ["オセルタミビルリン酸塩", "アスピリン", "アミノ安息香酸エチル・パラブチルアミノ安息香酸ジエチルアミノエチル塩酸塩"]
@@ -20,7 +16,7 @@ class TableViewCell: UITableViewCell {
 
     var widths: [CGFloat] = [] 
     var isActive: Bool = false
-
+    var itemsSize: [CGFloat] = [100, 200, 300, 125]
     override func awakeFromNib() {
         super.awakeFromNib()
         setupCollectionView()
@@ -28,38 +24,7 @@ class TableViewCell: UITableViewCell {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-//        setupCollectionView()
-//            var frame = self.frame
-//            let newWidth = frame.width * 3
-//            value = newWidth
-//            frame.size.width = newWidth
-//            self.frame = frame
-//            self.scrollView.contentSize.width = newWidth
-//            self.tableView.contentSize.width = newWidth
-////            print(frame)
-//        let widthNew =  widths.reduce(0, +) + 110
-//        if widthNew > self.frame.width {
-//            self.scrollView.contentSize.width = widthNew
-//            self.csWidthTableView.constant = widthNew
-//            self.isActive = false
-//            self.tableView.reloadData()
-//        } else {
-//            self.isActive = true
-//            self.scrollView.contentSize.width = self.frame.width
-//            self.csWidthTableView.constant = self.frame.width
-//            self.tableView.reloadData()
-//        }
-//        print("Sonnv === layoutSubviews")
     }
-//
-//    func setupTableView() {
-//        self.scrollView.bounces = false
-//        self.tableView.delegate = self
-//        self.tableView.dataSource = self
-//        self.scrollView.delegate = self
-//        self.tableView.bounces = false
-//        self.tableView.register(UINib(nibName: "MutipleDrugsCountCell", bundle: nil), forCellReuseIdentifier: "MutipleDrugsCountCell")
-//    }
 
     func setupCollectionView() {
         self.collectionView.dataSource = self
@@ -67,92 +32,42 @@ class TableViewCell: UITableViewCell {
         self.collectionView.register(UINib(nibName: "CollectionViewCell2", bundle: nil),
                                      forCellWithReuseIdentifier: "CollectionViewCell2")
         if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-//            let width = UIScreen.main.bounds.width
             layout.minimumInteritemSpacing = 0
             layout.minimumLineSpacing = 0
-//            layout.itemSize = CGSize(width: width/3, height: 45)
-            layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         }
-//        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-//        layout.scrollDirection = .horizontal
-//        collectionView.collectionViewLayout = layout
-//
-//        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-//        layout.itemSize = CGSize(width: self.collectionView.frame.size.width / 5, height: self.collectionView.frame.size.height / 2.5)
-//        layout.minimumInteritemSpacing = 0
-//        layout.minimumLineSpacing = 0
+
     }
 }
 
-//extension TableViewCell: UIScrollViewDelegate {
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//
-//    }
-//}
-//
-//extension TableViewCell: UITableViewDataSource, UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 4
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "MutipleDrugsCountCell") as! MutipleDrugsCountCell
-//        cell.initStackView(drugNames, indexPath.row, isActive, widths)
-//        cell.widthView = {[weak self] widths in
-//            self?.widths.removeAll()
-//            self?.widths = widths
-//            self?.layoutIfNeeded()
-//        }
-//        print("Sonnv === cell")
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return  UITableView.automaticDimension
-//    }
-//}
-
 extension TableViewCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return itemsSize.count
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell2",
                                                       for: indexPath) as! CollectionViewCell2
-
-        if indexPath.row == 0 {
-            cell.setupStackView(drugNames, indexPath.row, [])
-//            cell.closure = {[weak self] width in
-//                self?.widths.append(width)
-//            }
-//            print(widths)
-        }
-        else {
-            cell.setupStackView(values, indexPath.row, widths)
-        }
         return cell
     }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width
-        return CGSize(width: width, height: 45)
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
     }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: itemsSize[indexPath.section], height: self.bounds.size.height)
     }
 }
 
